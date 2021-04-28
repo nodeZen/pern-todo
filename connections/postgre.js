4ddaf9b7bc0787d73cbbba248e965747e6be93c4
U S T S Venkatachalam Mokka <umokka@innominds.com>
2021-04-28 17:46:04 +0530

    Deploy

--- connections/postgre.js
+++ connections/postgre.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg");
 
-const devConnectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production"
-      ? process.env.DATABASE_URL
-      : devConnectionString,
+  connectionString: process.env.DATABASE_URL
+    
 });
 
 module.exports = pool;
