6f79045b296ff32e406f5f20720ba6ab46412238
U S T S Venkatachalam Mokka <umokka@innominds.com>
2021-04-29 11:26:07 +0530

    Ready to deploy

--- connections/postgre.js
+++ connections/postgre.js
@@ -1,10 +1,21 @@
 const { Pool } = require("pg");
+const { parse } = require('pg-connection-string');
 
 require("dotenv").config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL+"?sslmode=require",
-  ssl:{rejectUnauthorized:false}
-});
+const getConfig = ()=>{
+  if(process.env.NODE_ENV === 'production'){
+    const config = parse(process.env.DATABASE_URL+"?sslmode=require");
+    config.ssl = {
+      rejectUnauthorized: false
+    }
+    return config;
+  }
+  return {
+    connectionString: process.env.DATABASE_URL
+  }
+}
+
+const pool = new Pool(getConfig());
 
 module.exports = pool;
